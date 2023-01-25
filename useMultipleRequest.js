75412ec866022321fc387542d8f7b5b3738880db
Vyshak <78524353+vyshak-sukumaran@users.noreply.github.com>
2023-01-25 12:01:55 +0530

    now working

--- useMultipleRequest.js
+++ useMultipleRequest.js
@@ -1,14 +1,20 @@
-const fetcher = (...urls) => {
-  const f = url => fetch(url).then(r => r.json())
-  return Promise.all(urls.map(url => f(url)))
-}
+import { useAxios } from "./useAxios";
+import useSWR from "swr";
 
-export default function useMultipleRequests() {
-  const urls = ['/api/v1/magazines/1234', '/api/v1/magazines/1234/articles']
-  const { data, error } = useSWR(urls, fetcher)
+export function useMultipleRequests(urls) {
+  const api = useAxios();
+  const fetcher = url =>
+    api(url).then((res) => res.data);
+
+    const multiFetcher = (resources, init) => {
+        console.log(resources, init);
+        return Promise.all(resources.map(url => fetcher(url)))
+    }
+  const { data, error, isLoading } = useSWR(urls, multiFetcher);
+
   return {
     data: data,
     isError: !!error,
-    isLoading: !data && !error
-  }
+    isLoading: isLoading,
+  };
 }
