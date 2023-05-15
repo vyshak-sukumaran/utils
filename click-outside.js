7282873b309862333d2be2c234535a508f49f2de
Vyshak <78524353+vyshak-sukumaran@users.noreply.github.com>
2023-05-15 18:49:04 +0530

    Create click-outside.js
    To capture click event that will only trigger when clicked outside of the element.

---
+++ click-outside.js
@@ -0,0 +1,11 @@
+dialog.addEventListener("click", e => {
+  const dialogDimensions = dialog.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close()
+  }
+})
