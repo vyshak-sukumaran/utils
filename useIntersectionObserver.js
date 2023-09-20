ae507b18b3d4adbdcbeb1e9d39260916c31584d6
Vyshak <78524353+vyshak-sukumaran@users.noreply.github.com>
2023-09-20 19:28:13 +0530

    Update useIntersectionObserver.js

--- useIntersectionObserver.js
+++ useIntersectionObserver.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-/** @param {React.MutableRefObject<HTMLDivElement>} ref */
-export const useIntersectionObserver = (
-  ref,
-  { threshold = 0, root = null, rootMargin = "0%", freezeOnceVisible = false }
+export const useIntersectionObserver = ({ threshold = 0, root = null, rootMargin = "0%", freezeOnceVisible = false }
 ) => {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-  const frozen = isIntersecting && freezeOnceVisible;
+    /** @type {React.MutableRefObject<HTMLElement>} */
+    const ref = useRef(null)
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const frozen = isIntersecting && freezeOnceVisible;
 
-  /** @param {IntersectionObserverEntry[]} [entry] */
-  const updateObserver = ([entry]) => {
-    setIsIntersecting(entry?.isIntersecting);
-  };
-  useEffect(() => {
-    const node = ref?.current;
-    const hasIOSupport = !!window.IntersectionObserver;
-
-    if (!hasIOSupport || frozen || !node) return;
-
-    const observerParams = { threshold, root, rootMargin };
-    const observer = new IntersectionObserver(updateObserver, observerParams);
-
-    observer.observe(node);
-    return () => {
-      observer.disconnect();
+    /** @param {IntersectionObserverEntry[]} [entry] */
+    const updateObserver = ([entry]) => {
+        setIsIntersecting(entry?.isIntersecting);
     };
-  }, [ref?.current, JSON.stringify(threshold), root, rootMargin, frozen]);
-  return { isIntersecting };
+    useEffect(() => {
+        const node = ref?.current;
+        const hasIOSupport = !!window.IntersectionObserver;
+
+        if (!hasIOSupport || frozen || !node) return;
+
+        const observerParams = { threshold, root, rootMargin };
+        const observer = new IntersectionObserver(updateObserver, observerParams);
+
+        observer.observe(node);
+        return () => {
+            observer.disconnect();
+        };
+    }, [ref?.current, JSON.stringify(threshold), root, rootMargin, frozen]);
+    return { ref, isIntersecting };
 };
