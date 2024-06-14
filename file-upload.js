3709e50d46392ed5b0a526b8de67ab35fec4fe1d
Vyshak <78524353+vyshak-sukumaran@users.noreply.github.com>
2024-06-14 16:14:24 +0530

    Update file-upload.js

--- file-upload.js
+++ file-upload.js
@@ -1,6 +1,7 @@
 import {
   ArrowUpTrayIcon,
   FolderIcon,
+  PaperClipIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { IconButton } from "@mui/material";
@@ -8,33 +9,40 @@ import React, { Fragment, useRef, useState, forwardRef } from "react";
 import { FILE_FORMATS, FILE_SIZE } from "../../utils";
 import { Button } from "./button";
 import PropTypes from "prop-types";
-import { useCustomTheme } from "../../context";
 
-let acceptedFormats = FILE_FORMATS.split("/").map(item => `.${item.toLowerCase()}`);
+let acceptedFormats = FILE_FORMATS.split("/").flatMap((item) => [
+  `.${item.toLowerCase()}`,
+  `.${item.toUpperCase()}`,
+]);
+let formats = FILE_FORMATS.split("/").map((item) => `.${item.toLowerCase()}`);
 
-const extensionRegex = /(?:\.([^.]+))?$/
+export const extensionRegex = /(?:\.([^.]+))?$/;
+
+export const getFileFormat = (fileName) =>
+  fileName.match(extensionRegex)[1] ?? "";
 
 const errMessages = {
   multiple: "Uploading multiple files are not currently available.",
-  format: `Please upload files in formats "${acceptedFormats.join()}"`,
-  size: "The size limit of the attachment is 10 MB. Reduce the file size and try again.",
+  format: `Please upload files in formats "${formats.join()}"`,
+  size: `The size limit of the attachment is ${FILE_SIZE ?? 10} MB. Reduce the file size and try again.`,
 };
 
 const additionalFormats = ",application/msword,application/vnd.ms-excel";
 acceptedFormats = acceptedFormats.join() + additionalFormats;
 
-export const FileUpload = forwardRef(function FileUpload({ value, setValue }, ref) {
+export const FileUpload = forwardRef(function FileUpload(
+  { value, setValue, buttonOnlyLayout = false, idPrefix = "", label = "" },
+  ref
+) {
+  const uploaderID = `${idPrefix}upload-file-wt`
   const [isDragging, setIsDragging] = useState(false);
   const [errMsg, setErrMsg] = useState(null);
-  const { theme } = useCustomTheme()
-
+  const limitFileSize = false;
   const internalInputRef = useRef(null);
   /**
-  * @type {React.RefObject<HTMLInputElement>}
-  */
-  const resolvedRef = ref || internalInputRef
-
-  const getFileFormat = (fileName) => fileName.match(extensionRegex)[1]
+   * @type {React.RefObject<HTMLInputElement>}
+   */
+  const resolvedRef = ref || internalInputRef;
 
   const handleFiles = (f) => {
     const files = Array.from(f);
@@ -53,7 +61,7 @@ export const FileUpload = forwardRef(function FileUpload({ value, setValue }, re
       if (!availableFormats.includes(fileFormat)) {
         setErrMsg(errMessages.format);
         return;
-      } else if (sizeToMB >= availableFileSize) {
+      } else if (sizeToMB >= availableFileSize && limitFileSize) {
         setErrMsg(errMessages.size);
         return;
       } else {
@@ -82,19 +90,23 @@ export const FileUpload = forwardRef(function FileUpload({ value, setValue }, re
   };
 
   const isValidExtension = (files) => {
-    if (!files.length) return false
+    if (!files.length) return false;
     for (const file of files) {
       if (!acceptedFormats.includes(getFileFormat(file.name))) {
-        setErrMsg(errMessages.format)
-        return false
+        setErrMsg(errMessages.format);
+        return false;
       }
     }
-    return true
-  }
+    return true;
+  };
 
   const handleChange = (e) => {
     e.preventDefault();
-    if (e.target.files && e.target.files[0] && isValidExtension(e.target.files)) {
+    if (
+      e.target.files &&
+      e.target.files[0] &&
+      isValidExtension(e.target.files)
+    ) {
       handleFiles(e.target.files);
     }
   };
@@ -118,7 +130,11 @@ export const FileUpload = forwardRef(function FileUpload({ value, setValue }, re
     e.preventDefault();
     e.stopPropagation();
     setIsDragging(false);
-    if (e.dataTransfer.files && e.dataTransfer.files[0] && isValidExtension(e.dataTransfer.files)) {
+    if (
+      e.dataTransfer.files &&
+      e.dataTransfer.files[0] &&
+      isValidExtension(e.dataTransfer.files)
+    ) {
       handleFiles(e.dataTransfer.files);
     }
   };
@@ -133,7 +149,8 @@ export const FileUpload = forwardRef(function FileUpload({ value, setValue }, re
     setErrMsg(null);
   };
   const handlePaste = (event) => {
-    const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    const items = (event.clipboardData || event.originalEvent.clipboardData)
+      .items;
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
       if (item.type.indexOf("image") !== -1) {
@@ -143,94 +160,125 @@ export const FileUpload = forwardRef(function FileUpload({ value, setValue }, re
           type: "image/png",
           lastModified: Date.now(),
         });
-        handleFiles([file])
+        handleFiles([file]);
       }
     }
-  }
+  };
 
-  const showError = (title) => (
+  const ViewError = ({ errMsg }) => (errMsg &&
     <Fragment>
       <div
         className={`flex gap-2 items-center justify-center pt-2 text-red-500`}
       >
-        <p className="text-sm">{title}</p>
+        <p className="text-sm">{errMsg}</p>
         <IconButton size="small" onClick={handleClear}>
           <XMarkIcon className="w-4 h-4 text-gray-500" />
         </IconButton>
       </div>
     </Fragment>
   );
-  const showFiles = (title) => (
-    <Fragment>
-      <div className="flex gap-1 items-center bg-sky bg-opacity-10 rounded-full p-1 pl-3 w-fit">
-        <p className="text-sm max-w-[180px] text-gray-500 overflow-hidden whitespace-nowrap text-ellipsis">
-          {title}
-        </p>
-        <IconButton size="small" onClick={() => handleRemoveFile(title)}>
-          <XMarkIcon className="w-4 h-4 text-gray-500" />
-        </IconButton>
-      </div>
-    </Fragment>
-  );
 
-  return (
-    <div className={`border-[1px] p-2 rounded-md min-h-[150px] grow ${theme === "Dark" ? "border-gray-600 hover:border-gray-500" : "border-gray-300 hover:border-gray-400"}`}>
+  const ViewFilesList = ({ files, noMargin }) => (files && (
+    <ul className={`list-none flex gap-2 flex-wrap ${!noMargin && "mt-2"}`}>
+      {files?.map((item, index) => (
+        <li key={item.name + index}>
+          <div className="flex gap-1 items-center bg-sky bg-opacity-10 rounded-full p-1 pl-3 w-fit">
+            <p className="text-sm max-w-[180px] text-gray-500 dark:text-slate-100 overflow-hidden whitespace-nowrap text-ellipsis">
+              {item.name}
+            </p>
+            <IconButton size="small" onClick={() => handleRemoveFile(item.name)}>
+              <XMarkIcon className="w-4 h-4 text-gray-500 dark:text-slate-100" />
+            </IconButton>
+          </div>
+        </li>
+      ))}
+    </ul>
+  ))
+
+  const DragOverlay = ({ isDragging }) => (isDragging && (
+    <div
+      className="absolute inset-0 w-full h-full"
+      onDragEnter={handleDrag}
+      onDragLeave={handleDrag}
+      onDragOver={handleDrag}
+      onDrop={handleDrop}
+    ></div>
+  ))
+
+  const MainContentSection = () => (
+    <div className="flex flex-col justify-center items-center gap-2 text-gray-500">
+      <ArrowUpTrayIcon className="w-10 h-10 text-gray-400" />
+      <p className="text-gray-400">
+        Drag and Drop files here
+        {!limitFileSize ? "" : " (maximum {FILE_SIZE}MB)"}.
+      </p>
+      <div className="flex gap-2 items-center">
+        <span>Or</span>
+        <Button
+          variant="contained"
+          color="sky"
+          startIcon={<FolderIcon className="text-white w-5 h-5" />}
+          type="button"
+          onClick={handleButtonClick}
+        >
+          Browse
+        </Button>
+      </div>
+      <p className="text-sm text-gray-400">
+        ( {"'Ctrl+V'"} to paste the copied content )
+      </p>
+    </div>
+  )
+
+  const FileInput = () => (
+    <input
+      ref={resolvedRef}
+      type="file"
+      onChange={handleChange}
+      name={uploaderID}
+      id={uploaderID}
+      accept={acceptedFormats}
+      hidden
+    />
+  )
+
+  return !buttonOnlyLayout ? (
+    <div
+      className="border-[1px] p-2 rounded-md min-h-[150px] grow dark:border-gray-600 dark:hover:border-gray-500 border-gray-300 hover:border-gray-400">
       <div
         onDragEnter={handleDrag}
         onPaste={handlePaste}
-        className={`border-2 relative rounded-md border-dashed p-5 w-full h-full flex flex-col gap-2 items-center justify-center transition-colors duration-200 ease-in-out ${isDragging && "bg-sky bg-opacity-10"} ${theme === "Dark" ? "border-slate-500" : "border-slate-400"}`}
+        className={`border-2 relative rounded-md border-dashed p-5 w-full h-full flex flex-col gap-2 items-center justify-center transition-colors duration-200 ease-in-out ${isDragging && "bg-sky bg-opacity-10"
+          } dark:border-slate-500 border-slate-400`}
       >
-        <input
-          ref={resolvedRef}
-          type="file"
-          onChange={handleChange}
-          name="upload-file-wt"
-          id="upload-file-wt"
-          accept={acceptedFormats}
-          hidden
-        />
-        <div className="flex flex-col justify-center items-center gap-2 text-gray-500">
-          <ArrowUpTrayIcon className="w-10 h-10 text-gray-400" />
-          <p className="text-gray-400">Drag and Drop files here (maximum {FILE_SIZE}MB).</p>
-          <div className="flex gap-2 items-center">
-            <span>Or</span>
-            <Button
-              variant="contained"
-              color="secondary"
-              startIcon={<FolderIcon className="text-white w-5 h-5" />}
-              type="button"
-              onClick={handleButtonClick}
-            >
-              Browse
-            </Button>
-          </div>
-          <p className="text-sm text-gray-400">( {"'Ctrl+V'"} to paste the copied content )</p>
-        </div>
-        {errMsg && showError(errMsg)}
-        {value && (
-          <ul className="list-none flex gap-2 flex-wrap mt-2">
-            {value?.map((item, index) => (
-              <li key={item.name + index}>{showFiles(item.name)}</li>
-            ))}
-          </ul>
-        )}
-        {isDragging && (
-          <div
-            className="absolute inset-0 w-full h-full"
-            onDragEnter={handleDrag}
-            onDragLeave={handleDrag}
-            onDragOver={handleDrag}
-            onDrop={handleDrop}
-          ></div>
-        )}
+        <FileInput />
+        <MainContentSection />
+        <ViewError errMsg={errMsg} />
+        <ViewFilesList files={value} />
+        <DragOverlay isDragging={isDragging} />
       </div>
     </div>
+  ) : (
+    <div className="flex items-center gap-2 flex-wrap">
+      <ViewFilesList files={value} noMargin />
+      <div className="h-fit w-fit">
+        <label htmlFor={uploaderID} className="flex gap-2 bg-blue p-[6px] pr-2 text-xs items-center rounded-full bg-opacity-10 text-blue dark:text-sky border-[1px] border-blue dark:border-sky hover:cursor-pointer">
+          <PaperClipIcon className="w-4 h-4" />
+          <span>{label}</span>
+        </label>
+        <FileInput />
+      </div>
+      <ViewError errMsg={errMsg} />
+    </div>
   );
-})
+});
 
-FileUpload.displayName = 'FileUpload';
+FileUpload.displayName = "FileUpload";
 
 FileUpload.propTypes = {
   value: PropTypes.any,
   setValue: PropTypes.func.isRequired,
+  buttonOnlyLayout: PropTypes.bool,
+  idPrefix: PropTypes.string,
+  label: PropTypes.string,
 };
