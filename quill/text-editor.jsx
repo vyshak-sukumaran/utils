80d68442d868128a5505214a23db48fdf75d4f38
Vyshak <78524353+vyshak-sukumaran@users.noreply.github.com>
2024-06-25 12:49:10 +0530

    Update text-editor.jsx

--- quill/text-editor.jsx
+++ quill/text-editor.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, forwardRef } from "react";
 import "quill/dist/quill.snow.css";
 
 var formats = [
@@ -46,22 +46,24 @@ const quillOptions = {
   formats: formats,
 };
 
-export const RichTextArea = ({
-  theme,
-  value,
-  onChange,
-  onBlur = () => {},
-  readOnly = false,
-  limit,
-  placeholder = "",
-  width = 200,
-  fullWidth = false,
-  showCounter = false,
-  error = false,
-  noBorder = false,
-  helperText = "",
-  ...rest
-}) => {
+export const RichTextArea = forwardRef(function RTA(props, ref) {
+  let {
+    theme,
+    value,
+    onChange,
+    onBlur = () => {},
+    readOnly = false,
+    limit,
+    placeholder = "",
+    width = 200,
+    fullWidth = false,
+    showCounter = false,
+    error = false,
+    noBorder = false,
+    helperText = "",
+    ...rest
+  } = props;
+
   const [initialValueSet, setInitialValueSet] = useState(false);
 
   /**
@@ -91,115 +93,121 @@ export const RichTextArea = ({
           readOnly: readOnly,
         });
 
-        quillRef.current.clipboard.addMatcher("img", (_node, _delta) => {
+        quillRef.current?.clipboard?.addMatcher("img", (_node, _delta) => {
           const Delta = Quill.import("delta");
           return new Delta().insert("");
         });
-        quillRef.current.clipboard.addMatcher("picture", (_node, _delta) => {
+        quillRef.current?.clipboard?.addMatcher("picture", (_node, _delta) => {
           const Delta = Quill.import("delta");
           return new Delta().insert("");
         });
       }
       if (value && !initialValueSet) {
-        const cursorPosition = quillRef.current.getSelection()?.index;
+        const cursorPosition = quillRef.current?.getSelection()?.index;
 
-        if (quillRef.current.getLength() > 1) {
-          quillRef.current.setText("");
+        if (quillRef.current?.getLength() > 1) {
+          quillRef.current?.setText("");
         }
 
         try {
-          quillRef.current.setContents(JSON.parse(value));
+          quillRef.current?.setContents(JSON.parse(value));
         } catch {
-          quillRef.current.setText(value);
+          quillRef.current?.setText(value);
         }
 
         if (cursorPosition !== undefined) {
-          quillRef.current.setSelection(cursorPosition, 0);
+          quillRef.current?.setSelection(cursorPosition, 0);
         }
         // Update the state variable to indicate that the initial value has been set
         setInitialValueSet(true);
       }
+      try {
+        const toolbar = quillRef.current?.getModule("toolbar");
+        toolbar.addHandler("link", (value) => {
+          const modal = document.querySelector(`div[data-modalBox="true"]`);
+          if (value) {
+            let urlLabel = document.createElement("span");
+            urlLabel.textContent = "Enter link";
+            let urlInput = document.createElement("input");
+            urlInput.setAttribute("type", "text");
+            urlInput.setAttribute("tabindex", "-1");
+            let okButton = document.createElement("button");
+            okButton.classList.add("ql-custom-prompt-ok-button");
+            okButton.textContent = "OK";
+            let cancelButton = document.createElement("button");
+            cancelButton.textContent = "Cancel";
 
-      const toolbar = quillRef.current.getModule("toolbar");
-      toolbar.addHandler("link", (value) => {
-        if (value) {
-          let urlLabel = document.createElement("span");
-          urlLabel.textContent = "Enter link";
-          let urlInput = document.createElement("input");
-          urlInput.setAttribute("type", "text");
-          let okButton = document.createElement("button");
-          okButton.classList.add("ql-custom-prompt-ok-button");
-          okButton.textContent = "OK";
-          let cancelButton = document.createElement("button");
-          cancelButton.textContent = "Cancel";
+            let promptBoxBackdrop = document.createElement("div");
+            promptBoxBackdrop.classList.add("ql-custom-prompt-box-backdrop");
 
-          let promptBoxBackdrop = document.createElement("div");
-          promptBoxBackdrop.classList.add("ql-custom-prompt-box-backdrop");
-
-          let promptBox = document.createElement("div");
-          promptBox.classList.add("ql-custom-prompt-box");
-          if (theme === "Dark") {
-            promptBox.classList.add(theme);
-          }
-
-          promptBox.appendChild(urlLabel);
-          promptBox.appendChild(urlInput);
-          promptBox.appendChild(okButton);
-          promptBox.appendChild(cancelButton);
-
-          okButton.onclick = function () {
-            let url = urlInput.value;
-            if (url) {
-              quillRef.current.format("link", url);
+            let promptBox = document.createElement("div");
+            promptBox.classList.add("ql-custom-prompt-box");
+            if (theme === "Dark") {
+              promptBox.classList.add(theme);
             }
-            document.body.removeChild(promptBox);
-            document.body.removeChild(promptBoxBackdrop);
-          };
 
-          cancelButton.onclick = function () {
-            document.body.removeChild(promptBox);
-            document.body.removeChild(promptBoxBackdrop);
-          };
+            promptBox.appendChild(urlLabel);
+            promptBox.appendChild(urlInput);
+            promptBox.appendChild(okButton);
+            promptBox.appendChild(cancelButton);
 
-          document.body.appendChild(promptBoxBackdrop);
-          document.body.appendChild(promptBox);
-        } else {
-          try {
-            quillRef.current?.format("link", false);
-          } catch {}
-        }
-      });
+            okButton.onclick = function () {
+              console.log(document.activeElement);
+              let url = urlInput.value;
+              if (url) {
+                quillRef.current?.format("link", url);
+              }
+              document.body?.removeChild(promptBox);
+              document.body?.removeChild(promptBoxBackdrop);
+              if (modal) {
+                modal.setAttribute("tabIndex", "-1");
+              }
+            };
+
+            cancelButton.onclick = function () {
+              document.body?.removeChild(promptBox);
+              document.body?.removeChild(promptBoxBackdrop);
+              if (modal) {
+                modal.setAttribute("tabIndex", "-1");
+              }
+            };
+
+            document.body?.appendChild(promptBoxBackdrop);
+            document.body?.appendChild(promptBox);
+            if (modal) {
+              modal.removeAttribute("tabIndex");
+            }
+          } else {
+            try {
+              quillRef.current?.format("link", false);
+            } catch {}
+          }
+        });
+      } catch (error) {
+        console.log(error, "Quill Error");
+        return;
+      }
       const textChangeHandler = () => {
-        if (limit && quillRef.current.getLength() > limit) {
-          quillRef.current.deleteText(limit, quillRef.current.getLength());
+        if (limit && quillRef.current?.getLength() > limit) {
+          quillRef.current?.deleteText(limit, quillRef.current?.getLength());
         }
 
-        if (quillRef.current.getLength() - 1 === 0) {
+        if (quillRef.current?.getLength() - 1 === 0) {
           onChange("");
         } else {
-          onChange(JSON.stringify(quillRef.current.getContents()));
+          onChange(JSON.stringify(quillRef.current?.getContents()));
         }
       };
 
-      quillRef.current.on("text-change", textChangeHandler);
+      quillRef.current?.on("text-change", textChangeHandler);
 
       return () => {
-        quillRef.current.off("text-change", textChangeHandler);
+        quillRef.current?.off("text-change", textChangeHandler);
       };
     };
 
     initializeQuill();
-  }, [
-    editorRef,
-    quillRef,
-    value,
-    readOnly,
-    onChange,
-    initialValueSet,
-    placeholder,
-    limit,
-    theme,
-  ]);
+  }, [editorRef, quillRef, value, readOnly, onChange, initialValueSet, placeholder, limit, theme]);
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -254,51 +262,61 @@ export const RichTextArea = ({
     };
   }, [containerRef, onBlur]);
 
-  const currentLength = quillRef.current ? quillRef.current.getLength() - 1 : 0;
-  const textColor = theme === "Dark" ? "text-gray-300" : "text-gray-500";
+  const currentLength = quillRef.current
+    ? quillRef.current?.getLength() - 1
+    : 0;
 
   const border = noBorder
     ? ""
     : `border-[1px] focus-within:border-blue ${
         error
-          ? "border-red-500"
-          : theme === "Dark"
-          ? "border-gray-500"
-          : "border-gray-300"
+          ? "dark:border-[#f44336] border-[#d32f2f]"
+          : "dark:border-slate-500 border-gray-300"
       }`;
   return (
-    <div className="relative" onDrop={(e) => e.preventDefault()}>
+    <div ref={ref} className="relative" onDrop={(e) => e.preventDefault()}>
       <div
         ref={containerRef}
-        className={`ql-custom-wrapper ${theme} ${width} ${border} relative min-h-[80px] flex flex-col-reverse rounded-md cursor-text`}
+        className={`ql-custom-wrapper ${theme} ${width} ${border} ${
+          error && "ql-error"
+        } relative min-h-[80px] flex flex-col-reverse rounded-md cursor-text`}
         {...rest}
       >
         <div ref={editorRef} className={`${theme}`}></div>
       </div>
       <div className="text-xs w-full">
         {helperText && (
-          <div className={`${error ? "text-red-500" : textColor} pt-1`}>
+          <div
+            className={`${
+              error
+                ? "dark:text-[#f44336] text-[#d32f2f]"
+                : "dark:text-gray-300 text-gray-500"
+            } pt-1 ml-2 font-medium`}
+          >
             {helperText}
           </div>
         )}
         {showCounter && (
-          <span className={`ml-auto w-fit block mt-1 ${textColor}`}>
+          <span
+            className={`ml-auto w-fit block mt-1 dark:text-slate-200 "dark:text-gray-300 text-gray-500"`}
+          >
             {currentLength}/{limit}
           </span>
         )}
       </div>
     </div>
   );
-};
+});
 
-export const RichTextAreaViewer = ({
-  content = "",
-  theme,
-  fullWidth,
-  width = 300,
-  noBorder = false,
-  ...rest
-}) => {
+export const RichTextAreaViewer = forwardRef(function RTAViewer(props, ref) {
+  let {
+    content = "",
+    theme,
+    fullWidth,
+    width = 300,
+    noBorder = false,
+    ...rest
+  } = props;
   const [initialValueSet, setInitialValueSet] = useState(false);
   width = fullWidth ? "w-full" : `w-[${width}px]`;
   /**
@@ -320,20 +338,20 @@ export const RichTextAreaViewer = ({
       }
 
       if (content && !initialValueSet) {
-        const cursorPosition = quillRef.current.getSelection()?.index;
+        const cursorPosition = quillRef.current?.getSelection()?.index;
 
-        if (quillRef.current.getLength() > 1) {
-          quillRef.current.setText("");
+        if (quillRef.current?.getLength() > 1) {
+          quillRef.current?.setText("");
         }
 
         try {
-          quillRef.current.setContents(JSON.parse(content));
+          quillRef.current?.setContents(JSON.parse(content));
         } catch {
-          quillRef.current.setText(content);
+          quillRef.current?.setText(content);
         }
 
         if (cursorPosition !== undefined) {
-          quillRef.current.setSelection(cursorPosition, 0);
+          quillRef.current?.setSelection(cursorPosition, 0);
         }
         // Update the state variable to indicate that the initial value has been set
         setInitialValueSet(true);
@@ -342,13 +360,11 @@ export const RichTextAreaViewer = ({
     initializeQuill();
   }, [editorRef, quillRef, content, initialValueSet]);
 
-  const border = noBorder
-    ? ""
-    : `border-[1px] focus-within:border-blue ${
-        theme === "Dark" ? "border-gray-500" : "border-gray-300"
-      }`;
+  const border = !noBorder
+    ? `border-[1px] focus-within:border-blue dark:border-gray-500 border-gray-300`
+    : "";
   return (
-    <div className="relative">
+    <div ref={ref} className="relative">
       <div
         className={`ql-custom-wrapper ${theme} ${width} ${border} relative min-h-[80px] rounded-md cursor-text `}
         {...rest}
@@ -357,4 +373,4 @@ export const RichTextAreaViewer = ({
       </div>
     </div>
   );
-};
+});
